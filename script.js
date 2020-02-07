4a24c7b4c5e1dc503d162ce0b31d116747182bfb
vitya1995 <51419053+Vityalol1995@users.noreply.github.com>
2020-02-07 17:23:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.querySelector('.btn').onclick = function(){
+    document.querySelector(".step__one").style.display = "none";
+    if(document.querySelector(".step__one").style.display = "none"){
+        document.querySelector(".step__two").style.display = "block";
+    }
+};
+document.querySelector('.step__two-btn').onclick = function(){
+   document.querySelector(".step__two").style.display = "none";
+   if(document.querySelector(".step__two").style.display = "none"){
+        document.querySelector(".question__1").style.display = "block";
+    }
+};
+document.querySelector('.question__submit1').onclick = function(){
+   document.querySelector(".question__1").style.display = "none";
+   if(document.querySelector(".question__1").style.display = "none"){
+        document.querySelector(".question__2").style.display = "block";
+    }
+};
+document.querySelector('.question__submit2').onclick = function(){
+   document.querySelector(".question__2").style.display = "none";
+   if(document.querySelector(".question__2").style.display = "none"){
+        document.querySelector(".question__3").style.display = "block";
+    }
+};
+document.querySelector('.question__submit3').onclick = function(){
+   document.querySelector(".question__3").style.display = "none";
+   if(document.querySelector(".question__3").style.display = "none"){
+        document.querySelector(".question__4").style.display = "block";
+    }
+};
+document.querySelector('.question__submit4').onclick = function(){
+   document.querySelector(".question__4").style.display = "none";
+   if(document.querySelector(".question__4").style.display = "none"){
+        document.querySelector(".options__1").style.display = "block";
+    }
+};
+document.querySelector('.next--btn1').onclick = function(){
+   document.querySelector(".options__1").style.display = "none";
+   if(document.querySelector(".options__1").style.display = "none"){
+        document.querySelector(".options__2").style.display = "block";
+    }
+};
+document.querySelector('.next--btn2').onclick = function(){
+   document.querySelector(".options__2").style.display = "none";
+   if(document.querySelector(".options__2").style.display = "none"){
+        document.querySelector(".options__3").style.display = "block";
+    }
+};
+document.querySelector('.next--btn3').onclick = function(){
+   document.querySelector(".options__3").style.display = "none";
+   if(document.querySelector(".options__3").style.display = "none"){
+        document.querySelector(".thank").style.display = "block";
+    }
+};
+
+
+
